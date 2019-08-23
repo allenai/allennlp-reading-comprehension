f7518015920211d9fa5cb3c6f949b0109661b077
Nelson Liu <nfliu@nelsonliu.me>
2019-08-22 20:03:55 -0400

    Remove test classes

--- allennlp_reading_comprehension/common/testing/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-Utilities and helpers for writing tests.
-"""
-from allennlp_reading_comprehension.common.testing.test_case import AllenNlpReadingComprehensionTestCase
-from allennlp_reading_comprehension.common.testing.model_test_case import ReadingComprehensionModelTestCase
--- allennlp_reading_comprehension/common/testing/model_test_case.py
+++
@@ -1,6 +0,0 @@
-from allennlp.common.testing import ModelTestCase
-from allennlp_reading_comprehension.common.testing import AllenNlpReadingComprehensionTestCase
-
-
-class ReadingComprehensionModelTestCase(AllenNlpReadingComprehensionTestCase, ModelTestCase):
-    pass
